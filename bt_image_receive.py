be1b60d7b142881ae76bfafa04352bd9ae9c450e
Jonathan <jonathan@ihle.in>
2017-08-12 12:53:19 +0800

    Comments
    Added explanatory comments

--- bt_image_receive.py
+++ bt_image_receive.py
@@ -1,10 +1,14 @@
 from microbit import *
 import radio
 
+#Turns on the bluetooth radio. FYI: this will drain the battery faster than normal operations.
 radio.on()
 
+#An infinite loop
 while True:
+    #Listen for a bluetooth broadcast and save it to a string.
     image = radio.receive()
+    #Check that the string is set to a value.
     if image is not None:
         i = Image(image)
         display.show(i)
--- bt_image_send.py
+++ bt_image_send.py
@@ -1,25 +1,32 @@
 from microbit import *
 import radio
 
+#Simple function to turn built-in images to strings - parameter is a built-in image object
 def image_to_string(source):
+    #Convert the image to a string.
     target = str(source)
+    #Format the string as a custom image string.
     target = target.replace("Image(","")
     target = target.replace("\n", "")
     target = target.replace("'", "")
     target = target.replace(" ", "")
     target = target.replace(")", "")
+    #Slice off the last character.
     target = target[:-1]
     return target
-    
+
+#An arbitrary list of built-in images to choose from.    
 images = [Image.ANGRY, Image.ASLEEP, Image.CHESSBOARD, Image.CONFUSED, Image.COW, Image.FABULOUS, Image.GHOST]
 image_number = -1
 
 while True:
+    #Allows the user to cycle through the preset list of images.
     if button_a.was_pressed():
         image_number = image_number + 1
         if image_number >= len(images):
             image_number = 0
         display.show(images[image_number])
+    #Takes the currently displayed image, converts it to a string and broadcasts.
     if button_b.was_pressed():
         message = image_to_string(images[image_number])
         radio.on()
